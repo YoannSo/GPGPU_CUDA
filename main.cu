be6d21f34fca0b312bd5e9fe3c5e1287365cba65
Sochaj Yoann <yoann.sochaj@etu.unilim.fr>
2021-11-21 22:39:58 +0100

    mise en place

--- main.cu
+++ main.cu
@@ -13,7 +13,91 @@
 #define PI 3.141592
 using namespace std;
 
-void RGBtoHSV (int* rgb, vector<float>& h, vector<float>& s, vector<float>& v,int size){
+vector<int> workOnCpu(vector<int> rgb);
+void RGBtoHSV_CPU (int* rgb, vector<float>& h, vector<float>& s, vector<float>& v,int size);
+void HSVtoRGB_CPU(float* h,float *s,float*v,int size,vector<int>&rgb);
+int* histo_CPU(float*v,int size);
+int* repartition_CPU(int* histo,int size);
+vector<float> egalisation_CPU(int* repartition, vector<float> v);
+
+vector<int> workOnGpu(vector<int> rgb);
+void RGBtoHSV_GPU(vector<int> rgb,vector<float>&h,vector<float>&s,vector<float>&v);
+int* computeHistogram_GPU(vector<float>v);
+int* repartition_GPU(int* histo,int size);
+vector<float> egalisation_GPU(int* repartition,vector<float>v);
+void HSVtoRGB_GPU(vector<float> h,vector<float>s,vector<float>v,vector<int>&rgb);
+
+
+int main(){
+    cout<<"hello world"<<endl;
+    Image img=Image();
+    img.load("img/Chateau.png");
+    vector<int> newRGB_CPU=workOnCpu(img.getPixelRGB());
+    img.setRGB(newRGB_CPU);
+    img.setPixels();
+    img.save("img/ChateauCPU.png");
+
+    img.load("img/Chateau.png");
+    vector<int> newRGB_GPU=workOnGpu(img.getPixelRGB());
+    img.setRGB(newRGB_GPU);
+    img.setPixels();
+    img.save("img/ChateauGPU.png");
+       
+}
+
+
+///////////////////////// CPU PART /////////////////////////////////////////
+vector<int> workOnCpu(vector<int> rgb){
+    float totalTime=0;
+    vector<float> h=vector<float>();
+    vector<float> s=vector<float>();
+    vector<float> v=vector<float>();
+    ChronoCPU chrCPU;
+
+    cout << "============================================"	<< endl;
+	cout << "         Sequential version on CPU          "	<< endl;
+	cout << "============================================"	<< endl;
+    chrCPU.start();
+    RGBtoHSV_CPU(rgb.data(),h,s,v,rgb.size());
+    chrCPU.stop();
+    totalTime += chrCPU.elapsedTime();
+    cout << "-> RGB to HSV done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
+    chrCPU.start();
+
+    int* histogram=new int[256];
+    histogram=histo_CPU(v.data(),v.size());
+    
+    chrCPU.stop();
+    cout << "-> Compute histogram done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrCPU.elapsedTime();
+    chrCPU.start();
+
+    int* repartition=new int[256];
+    repartition=repartition_CPU(histogram,256);
+
+    chrCPU.stop();
+    cout << "-> Compute Repartition done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrCPU.elapsedTime();    
+    chrCPU.start();
+
+    vector<float> newV=vector<float>();
+    newV = egalisation_CPU(repartition, v); 
+
+    chrCPU.stop();
+    cout << "-> Compute Egalisation histograme done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrCPU.elapsedTime();
+    chrCPU.start();
+
+    vector<int> newRGB;
+    HSVtoRGB_CPU(h.data(), s.data(), newV.data(), h.size(), newRGB);
+
+    chrCPU.stop();
+    cout << "-> HSV to RGB done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrCPU.elapsedTime();
+    cout << "-> ALL CPU DONE total time : " << fixed << setprecision(2) << totalTime<< " ms" << endl << endl;
+    return newRGB;
+}
+void RGBtoHSV_CPU (int* rgb, vector<float>& h, vector<float>& s, vector<float>& v,int size){
     float r,g,b;
     float cMax,cMin,delta;
 
@@ -62,8 +146,7 @@ void RGBtoHSV (int* rgb, vector<float>& h, vector<float>& s, vector<float>& v,in
     }
 
 }
-
-void HSVtoRGB(float* h,float *s,float*v,int size,vector<int>&rgb){
+void HSVtoRGB_CPU(float* h,float *s,float*v,int size,vector<int>&rgb){
    float c,x,absValue,m,hue,rTemp,gTemp,bTemp;
    for(int i=0;i<size;i++){
        hue=h[i];
@@ -114,7 +197,7 @@ void HSVtoRGB(float* h,float *s,float*v,int size,vector<int>&rgb){
    }
 }
 
-int* histoCPU(float*v,int size){
+int* histo_CPU(float*v,int size){
         int* histogram=new int[256]; // de 0 a 255 valeur de V
         for(int i=0;i<256;i++){
             histogram[i]=0;
@@ -125,7 +208,7 @@ int* histoCPU(float*v,int size){
         }
         return histogram;
 }
-int* repartitionCPU(int* histo,int size){
+int* repartition_CPU(int* histo,int size){
         int* repartition=new int[256]; // de 0 a 255 valeur de V
         int histoValue;
         for(int i=0;i<size;i++){
@@ -141,7 +224,8 @@ int* repartitionCPU(int* histo,int size){
         return repartition;
 }
 
-vector<float> egalisationCPU(int* repartition, vector<float> v) {
+vector<float> egalisation_CPU(int* repartition, vector<float> v) {
+
     float* egalisation = new float[256];
     vector<float> result= vector<float>(v.size());
     
@@ -157,65 +241,67 @@ vector<float> egalisationCPU(int* repartition, vector<float> v) {
     return result;
 }
 
-int main(){
-    cout<<"hello world"<<endl;
-    Image img=Image();
-    img.load("img/Chateau.png");
+
+//////////////////////////////////////////////// GPU PART //////////////////////////////////////////////////
+vector<int> workOnGpu(vector<int> rgb){
     float totalTime=0;
-    vector<int> rgb;
     vector<float> h=vector<float>();
     vector<float> s=vector<float>();
     vector<float> v=vector<float>();
-    rgb = img.getPixelRGB();    
+    vector<int> newRGB=vector<int>();
+    int* histo=new int[256];
+    int* repartition=new int[256];
+    vector<float>newV=vector<float>();
+    ChronoGPU chrGPU;
 
     cout << "============================================"	<< endl;
-	cout << "         Sequential version on CPU          "	<< endl;
+	cout << "         Parallel version on GPU          "	<< endl;
 	cout << "============================================"	<< endl;
-    ChronoCPU chrCPU;
-    chrCPU.start();
-    RGBtoHSV(rgb.data(),h,s,v,rgb.size());
-    chrCPU.stop();
-    totalTime += chrCPU.elapsedTime();
-    cout << "-> RGB to HSV done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
-    chrCPU.start();
-
-    int* histogram=new int[256];
-    histogram=histoCPU(v.data(),v.size());
     
-    chrCPU.stop();
-    cout << "-> Compute histogram done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
-    totalTime += chrCPU.elapsedTime();
-    chrCPU.start();
+    chrGPU.start();
+    RGBtoHSV_GPU(rgb,h,s,v);
+    chrGPU.stop();
+    cout << "-> RGB to HSV done : " << fixed << setprecision(2) << chrGPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrGPU.elapsedTime();    
+  
+    chrGPU.start();
+    histo=computeHistogram_GPU(v);
+    chrGPU.stop();
+    cout << "-> Compute histogram done : " << fixed << setprecision(2) << chrGPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrGPU.elapsedTime();
 
-    int* repartition=new int[256];
-    repartition=repartitionCPU(histogram,256);
+    chrGPU.start();
+    repartition=repartition_GPU(histo,256);
+    chrGPU.stop();
+    cout << "-> Compute Repartition done : " << fixed << setprecision(2) << chrGPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrGPU.elapsedTime();
 
-    chrCPU.stop();
-    cout << "-> Compute Repartition done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
-    totalTime += chrCPU.elapsedTime();    
-    chrCPU.start();
+    chrGPU.start();
+    newV=egalisation_GPU(histo,v);
+    chrGPU.stop();
+    cout << "-> Compute Egalisation done : " << fixed << setprecision(2) << chrGPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrGPU.elapsedTime();
 
-    vector<float> newV=vector<float>();
-    newV = egalisationCPU(repartition, v); 
-
-    chrCPU.stop();
-    cout << "-> Compute Egalisation histograme done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
-    totalTime += chrCPU.elapsedTime();
-    chrCPU.start();
-
-    vector<int> newRGB;
-    HSVtoRGB(h.data(), s.data(), newV.data(), h.size(), newRGB);
-
-    chrCPU.stop();
-    cout << "-> HSV to RGB done : " << fixed << setprecision(2) << chrCPU.elapsedTime() << " ms" << endl << endl;
-    totalTime += chrCPU.elapsedTime();
+    chrGPU.start();
+    HSVtoRGB_GPU(h,s,v,newRGB);
+    chrGPU.stop();
+    cout << "-> HSV to FINAL RGB done : " << fixed << setprecision(2) << chrGPU.elapsedTime() << " ms" << endl << endl;
+    totalTime += chrGPU.elapsedTime();
     cout << "-> ALL CPU DONE total time : " << fixed << setprecision(2) << totalTime<< " ms" << endl << endl;
+    return newRGB;
+}
+void RGBtoHSV_GPU(vector<int> rgb,vector<float>&h,vector<float>&s,vector<float>&v){
 
-    img.setRGB(newRGB);
-    img.setPixels();
-    img.save("img/testChateau.png");
-    
+}
+int* computeHistogram_GPU(vector<float>v){
 
-    
-    
+}
+int* repartition_GPU(int* histo,int size){
+
+}
+vector<float> egalisation_GPU(int* repartition,vector<float>v){
+
+}
+void HSVtoRGB_GPU(vector<float> h,vector<float>s,vector<float>v,vector<int>&rgb){
+
 }
